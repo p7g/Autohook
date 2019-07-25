ce0936bb32f991e57997b942288ef4b21a07863a
Patrick Gingras <775.pg.12@gmail.com>
2019-07-24 22:04:09 -0400

    implemented scripts by staged file extensions

--- autohook.sh
+++ autohook.sh
@@ -75,37 +75,74 @@ main() {
         repo_root=$(git rev-parse --show-toplevel)
         hook_type=$calling_file
         symlinks_dir="$repo_root/hooks/$hook_type"
-        files=("$symlinks_dir"/*)
-        number_of_symlinks="${#files[@]}"
-        if [[ $number_of_symlinks == 1 ]]
-        then
-            if [[ "$(basename ${files[0]})" == "*" ]]
-            then
-                number_of_symlinks=0
-            fi
-        fi
-        echo "Looking for $hook_type scripts to run...found $number_of_symlinks!"
-        if [[ $number_of_symlinks -gt 0 ]]
-        then
-            hook_exit_code=0
-            for file in "${files[@]}"
-            do
-                scriptname=$(basename $file)
-                echo "BEGIN $scriptname"
-                eval $file &> /dev/null
-                script_exit_code=$?
-                if [[ $script_exit_code != 0 ]]
-                then
-                  hook_exit_code=$script_exit_code
+
+        debug-print "[main] found repo root '$repo_root'"
+        debug-print "[main] hook type is '$hook_type'"
+        debug-print "[main] found symlinks dir '$symlinks_dir'"
+
+        staged_files=$(git diff --cached --name-only | rev | sort | rev)
+        debug-print "[main] staged files: '$staged_files'"
+        accumulator=
+        last_extension=
+
+        for staged_file in $staged_files ''; do # empty string allows us to run scripts for the last extension too
+            debug-print "[main] staged file: '$staged_file'"
+            current_extension=${staged_file##*.}
+            debug-print "[main] previous extension '$last_extension' -> current extension '$current_extension'"
+            if [ "$current_extension" != "$last_extension" ]; then
+                debug-print '[main] current extension is different from last extension'
+                if [ "$last_extension" != '' ]; then # don't trigger on the first loop
+                    debug-print '[main] last extension is not empty, running scripts'
+                    script_files=("$symlinks_dir/$last_extension"/*)
+                    number_of_symlinks="${#script_files[@]}"
+                    debug-print "[main] found $number_of_symlinks symlinks: '$script_files'"
+                    if [[ $number_of_symlinks == 1 ]]
+                    then
+                        if [[ "$(basename ${script_files[0]})" == "*" ]]
+                        then
+                            debug-print '[main] only script file was "*", setting number_of_symlinks to 0'
+                            number_of_symlinks=0
+                        fi
+                    fi
+                    echo "Looking for $hook_type scripts to run...found $number_of_symlinks!"
+                    if [[ $number_of_symlinks -gt 0 ]]
+                    then
+                        debug-print '[main] had symlinks, running scripts'
+                        hook_exit_code=0
+                        for file in "${script_files[@]}"
+                        do
+                            scriptname=$(basename $file)
+                            echo "BEGIN $scriptname for '$last_extension' extension"
+                            debug-print "[main] running '$file' with staged files '$accumulator'"
+                            result=$(2>&1 AUTOHOOK_HOOK_TYPE="$hook_type" AUTOHOOK_STAGED_FILES=$accumulator AUTOHOOK_REPO_ROOT="$repo_root" $file)
+                            script_exit_code=$?
+                            if [[ $script_exit_code != 0 ]]
+                            then
+                                debug-print "[main] script exited with $script_exit_code"
+                                hook_exit_code=$script_exit_code
+                            fi
+                            echo "FINISH $scriptname for '$last_extension' extension"
+                        done
+                        if [[ $hook_exit_code != 0 ]]
+                        then
+                            echo "A $hook_type script yielded negative exit code $hook_exit_code"
+                            printf "Result:\n%s\n" "$result"
+                            exit $hook_exit_code
+                        fi
+                    fi
                 fi
-                echo "FINISH $scriptname"
-            done
-            if [[ $hook_exit_code != 0 ]]
-            then
-              echo "A $hook_type script yielded negative exit code $hook_exit_code"
-              exit $hook_exit_code
+                if [ "$staged_file" == '' ]; then # end of staged files, break
+                    debug-print '[main] reached end of staged files'
+                    break
+                fi
+                last_extension=$current_extension
+                accumulator=
+                debug-print "[main] updating last extension to '$current_extension'"
             fi
-        fi
+            debug-print "[main] appending '$staged_file' to accumulator"
+            accumulator="$accumulator '$staged_file'"
+        done
+        debug-print '[main] finished'
     fi
 }
 
